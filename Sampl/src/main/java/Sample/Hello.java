51f895c9b9fba9e7003f0a7c7f6640780e3e52c2
girishbagineni <girishbagineni@gmail.com>
2020-07-31 12:58:27 +0530

    First Sample Commit

---
+++ Sampl/src/main/java/Sample/Hello.java
@@ -0,0 +1,9 @@
+package Sample;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello , just pushing the code into Git and running the scripts through jenkins");
+	}
+
+}
